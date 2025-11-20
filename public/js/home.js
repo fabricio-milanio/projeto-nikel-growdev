92f9434415e3bbf982182477cd159a122fb373ac
Fabricio Milanio <milanio.fabricio@gmail.com>
2025-11-20 14:31:35 -0300

    insere a lógica em javascript para mostrar as transações.

--- public/js/home.js
+++ public/js/home.js
@@ -22,6 +22,10 @@ function checkLogged() {
     if(dataUser) {
         data = JSON.parse(dataUser);
     }
+
+    getCashIn();
+    getCashOut();
+    getTotal();
 };
 
 function logout() {
@@ -31,6 +35,10 @@ function logout() {
 }
 
 document.getElementById("button-logout").addEventListener("click", logout);
+
+document.getElementById("transaction-button").addEventListener("click", function() {
+    window.location.href = 'transactions.html';
+});
 
 document.getElementById("transaction-form").addEventListener("submit", function(e) {
     e.preventDefault();
@@ -49,9 +57,105 @@ document.getElementById("transaction-form").addEventListener("submit", function(
     saveData(data);
     e.target.reset();
     myModal.hide();
+    getCashIn();
+    getCashOut();
+    getTotal();
     alert("Lançamento adicionado com sucesso!");
 });
 
 function saveData(data) {
     localStorage.setItem(logged, JSON.stringify(data));
+}
+
+function getCashIn() {
+    const transactions = data.transactions;
+    const cashIn = transactions.filter((item) => item.type === '1');
+    
+    if(cashIn.length) {
+        let cashInHtml = ``;
+        let limit = 0;
+
+        if(cashIn.length > 5) {
+            limit = 5;
+        } else {
+            limit = cashIn.length;
+        }
+        
+        for (let index = 0; index < limit; index++) {
+            cashInHtml += `
+                <div class="row mb-4">
+                    <div class="col-12">
+                        <h3 class="fs-2">R$${cashIn[index].value.toFixed(2)}</h3>
+                        <div class="container p-0">
+                            <div class="row">
+                                <div class="col-12 col-md-8">
+                                    <p>${cashIn[index].description}</p>
+                                </div>
+                                <div class="col-12 col-md-3 d-flex justify-content-end">
+                                    <p>${cashIn[index].date}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr>
+            `;
+        }
+
+        document.getElementById("cash-in-list").innerHTML = cashInHtml;
+    }
+}
+
+function getCashOut() {
+    const transactions = data.transactions;
+    const cashOut = transactions.filter((item) => item.type === '2');
+    
+    if(cashOut.length) {
+        let cashOutHtml = ``;
+        let limit = 0;
+
+        if(cashOut.length > 5) {
+            limit = 5;
+        } else {
+            limit = cashOut.length;
+        }
+        
+        for (let index = 0; index < limit; index++) {
+            cashOutHtml += `
+                <div class="row mb-4">
+                    <div class="col-12">
+                        <h3 class="fs-2">R$${cashOut[index].value.toFixed(2)}</h3>
+                        <div class="container p-0">
+                            <div class="row">
+                                <div class="col-12 col-md-8">
+                                    <p>${cashOut[index].description}</p>
+                                </div>
+                                <div class="col-12 col-md-3 d-flex justify-content-end">
+                                    <p>${cashOut[index].date}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr>
+            `;
+        }
+
+        document.getElementById("cash-out-list").innerHTML = cashOutHtml;
+    }
+}
+
+function getTotal() {
+    const transactions = data.transactions;
+    let total = 0;
+
+    transactions.forEach((item) => {
+        if(item.type === '1') {
+            total += item.value;
+        } else {
+            total -= item.value;
+        }
+    });
+
+    document.getElementById("total").innerText = `R$${total.toFixed(2)}`;
 }
